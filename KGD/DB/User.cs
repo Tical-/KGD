358f276f89093a202c99b470778366d4c8f19aaf
s.seidakhmetov <s.seidakhmetov@temabit.com>
2023-02-28 00:20:45 +0600

    sql with context added, need remove empty migration

---
+++ KGD/DB/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KGD
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
